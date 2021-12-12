11352dc5dbb8c0d2381b7d9694a976adf087cb14
iqbal2o2o <iqbalhossain1095@gmail.com>
2021-12-12 18:20:07 +0600

    dom menupulate

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -6,7 +6,7 @@ const ProductComponent = () => {
     const renderList =products.map((product)=>{
         const {id, title, image, price, category}=product;
         return(
-            <div className='four column wide' key={id}>
+            <div className='four wide column' key={id}>
             <div className='ui link cards'>
                 <div className='card'>
                     <div className="image">
@@ -15,7 +15,7 @@ const ProductComponent = () => {
                     <div className='content'>
                         <div className='header'>{title}</div>
                         <div className='meta price'>${price}</div>
-                        <div className='meta price'>{category}</div>
+                        <div className='meta'>{category}</div>
                     </div>
                 </div>
             </div>
