abddafe4fe20c05ea5163ef1c6ba2016c1177f63
iqbal2o2o <iqbalhossain1095@gmail.com>
2021-12-12 14:58:57 +0600

    apply react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Header from './containers/Header';
+import ProductDetails from './containers/ProductDetails';
+import ProductListing from './containers/ProductListing';
+
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>React Redux</h1>
-    </div>
+    <div className="App"> 
+    <Header/>
+    <Routes>           
+          <Route path="/" element={<ProductListing/>}/>
+          <Route path="/product/:productId" element={<ProductDetails/>}/>           
+      </Routes>       
+    </div>  
   ); 
 }
 
